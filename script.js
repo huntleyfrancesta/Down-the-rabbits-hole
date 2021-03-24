01220cac06eb417822682c2f7439d1383cff545e
francesta <huntleyfrancesta@gmail.com>
2021-03-23 22:56:14 -0400

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 let h2 = document.createElement('h2')
 h2.textContent = "What's life without change?"
 document.querySelector('body').appendChild(h2)
-const input = document.getElementById('input');
-input.addEventListener('click', function() {
-    alert('Alice and the wonderland');
-});
-input.addEventListener('click', clickAlert);
